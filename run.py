15e173c81f4f6d998014c009840a0f4ef41635f6
Mdsadeq123 <mdsadeq@example.com>
2026-08-22 15:06:51 +0530

    feat: add root entrypoint run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add project root directory to python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 import uvicorn
+from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
